1250920c160d3bddae47ad1163bdb157106e9736
tsquez <tsquez@gmail.com>
2013-09-08 08:23:39 -0700

    v1.0.0
    adjusted size of author gravatar and adjusted styles of author section

--- content.php
+++ content.php
@@ -42,14 +42,14 @@
 			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
 				<div class="author-info">
 					<div class="author-avatar">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'wpforge_author_bio_avatar_size', 68 ) ); ?>
+						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'wpforge_author_bio_avatar_size', 60 ) ); ?>
 					</div><!-- .author-avatar -->
 					<div class="author-description">
 						<h2><?php printf( __( 'About %s', 'wpforge' ), get_the_author() ); ?></h2>
 						<p><?php the_author_meta( 'description' ); ?></p>
 						<div class="author-link">
 							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'wpforge' ), get_the_author() ); ?>
+								<?php printf( __( 'View all posts by %s <span class="meta-nav">&raquo;</span>', 'wpforge' ), get_the_author() ); ?>
 							</a>
 						</div><!-- .author-link	-->
 					</div><!-- .author-description -->
