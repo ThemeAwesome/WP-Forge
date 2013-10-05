d27058c23925d9a3591eeff63acfce469eefb9a3
tsquez <tsquez@gmail.com>
2013-10-05 16:49:15 -0700

    v1.0.0
    updated to latest version of normalize.css, which is currently 2.1.3 -
    adjusted the function that wraps the flex-video class around iframe -
    moved css for element th and td to styles.css from the old normalize.css

--- js/functions.js
+++ js/functions.js
@@ -10,12 +10,8 @@ jQuery(document).foundation()
 	// Add button class to all submit buttons
 	jQuery('input[type="submit"]').addClass('button');
 	
-	// Adds flex video to embeded video: http://foundation.zurb.com/docs/elements.php
-	jQuery('iframe[src*="vimeo.com"]').wrap('<div class="flex-video widescreen" />');
-	jQuery('iframe[src*="dailymotion.com"]').wrap('<div class="flex-video widescreen" />');
-	jQuery('iframe[src*="metacafe.com"]').wrap('<div class="flex-video widescreen" />');
-	jQuery('iframe[src*="youtube.com"]').wrap('<div class="flex-video widescreen" />');
-	jQuery('iframe[src*="wordpress.tv"]').wrap('<div class="flex-video widescreen" />');	
+	// Adds flex video to embeded video: http://foundation.zurb.com/docs/components/flex-video.html
+	jQuery('iframe').wrap('<div class="flex-video widescreen" />');	
 	
 // end loading all functions   
    
