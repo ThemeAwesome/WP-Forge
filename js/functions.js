0c7f124c9f30afd758c98f79f27520daa7bbdb7b
tsquez <tsquez@gmail.com>
2013-10-09 06:23:52 -0700

    v1.0.0
    Added BackToTop functionality

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,27 @@ jQuery(document).foundation()
 	jQuery('input[type="submit"]').addClass('button');
 	
 	// Adds flex video to embeded video: http://foundation.zurb.com/docs/components/flex-video.html
-	jQuery('iframe').wrap('<div class="flex-video widescreen" />');	
+	jQuery('iframe').wrap('<div class="flex-video" />');	
+	
+	// BackToTop: make it fades in
+	jQuery(window).load(function() {
+		jQuery("#topofpage").hide().removeAttr("href");
+		if (jQuery(window).scrollTop() != "0")
+			jQuery("#backtotop").fadeIn("slow")
+		var scrollDiv = jQuery("#backtotop");
+		jQuery(window).scroll(function(){
+			if (jQuery(window).scrollTop() == "0")
+				jQuery(scrollDiv).fadeOut("slow")
+			else
+				jQuery(scrollDiv).fadeIn("slow")
+		});	
+	});	
+	// BacktoTop
+	jQuery('#backtotop').click(function(){
+		jQuery('html, body').animate({
+		scrollTop: jQuery("#header").offset().top
+		}, 2000);				   
+	});	
 	
 // end loading all functions   
    
