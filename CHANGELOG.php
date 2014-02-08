dee2232c1633c4b226fe2751c56653b823b69591
tsquez <tsquez@gmail.com>
2014-02-08 16:18:35 -0700

    v5.1.0
    date change

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -1,4 +1,4 @@
-<!-- Changelog as of 02/05/2014 -->
+<!-- Changelog as of 02/08/2014 -->
 
 1.) Updated WP-Forge to the latest version of Foundation, currently v5.1.0, released on 02/05/2014.
 2.) Added new menu location called social to functions.php (line 97)
